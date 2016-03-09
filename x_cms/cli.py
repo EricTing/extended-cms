66395a3d969b411188e9d4f80c35084d689855d7
EricTing <yunding.whu@gmail.com>
2016-03-09 13:26:11 -0600

    add command line options
    	modified:   x_cms/cli.py

--- x_cms/cli.py
+++ x_cms/cli.py
@@ -2,9 +2,13 @@ import click
 
 
 @click.command()
-@click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
-@click.argument('name', default='world', required=False)
-def main(name, as_cowboy):
+@click.option('--template-protein',
+              help='template protein file path, in PDB format')
+@click.option('--query-protein', help='query protein file path, in PDB format')
+@click.option('--query-ligand', help='query ligand file path, in SDF format')
+@click.option('--template-ligand',
+              help='template ligand file path, in SDF format')
+def main(template_protein, query_protein, template_ligand, query_ligand):
     """calculated extended contact mode score provided the query and template protein-ligand structures"""
-    greet = 'Howdy' if as_cowboy else 'Hello'
-    click.echo('{0}, {1}.'.format(greet, name))
+    click.echo('{0}, {1}, {2}, {3}'.format(template_protein, template_ligand,
+                                           query_protein, query_ligand))
